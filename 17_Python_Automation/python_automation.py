fea898492978e9a028f214acf2b44e21bd6d01c4
Ahmed Mohamed Mahmoud Rashad <68242300+ambou7a@users.noreply.github.com>
2023-01-17 23:36:26 +0200

    good morning text

---
+++ 17_Python_Automation/python_automation.py
@@ -0,0 +1,23 @@
+from credentials import phone_number
+import requests
+import schedule
+import time
+
+def send_message():
+    resp = requests.post('http://textbelt.com/text', {
+        'phone' : phone_number,
+        'message': 'Hey, Good morning',
+        'key': 'textbelt'
+    })
+    
+    print(resp.json())
+    
+#schedule.every().day.at('06:00').do(send_message)
+
+schedule.every(10).seconds.do(send_message)
+
+while True:
+    
+    schedule.run_pending()
+    time.sleep(1)
+    
